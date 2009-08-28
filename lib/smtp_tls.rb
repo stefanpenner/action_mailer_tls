c90b884e4626216aff379453d1fc40b6375bc8e3
stefanpenner <stefan.penner@gmail.com>
2009-08-28 09:51:55 -0700

    Should now work as expected in REE, jRuby and MRI

--- lib/smtp_tls.rb
+++ lib/smtp_tls.rb
@@ -1,19 +1,22 @@
 require "openssl"
 require "net/smtp"
-
+ 
 Net::SMTP.class_eval do
+  DEFAULT_AUTH_TYPE = :plain unless defined?(DEFAULT_AUTH_TYPE)
   private
-  def do_start(helodomain, user, secret, authtype)
+  def do_start(helodomain, user, secret, authtype = DEFAULT_AUTH_TYPE)
     raise IOError, 'SMTP session already started' if @started
-    check_auth_args user, secret, authtype if user or secret
-
+    if user or secret
+      check_auth_method(authtype)
+      check_auth_args(user, secret)
+    end
     sock = timeout(@open_timeout) { TCPSocket.open(@address, @port) }
     @socket = Net::InternetMessageIO.new(sock)
     @socket.read_timeout = 60 #@read_timeout
-
+ 
     check_response(critical { recv_response() })
     do_helo(helodomain)
-
+ 
     if starttls
       raise 'openssl library not installed' unless defined?(OpenSSL)
       ssl = OpenSSL::SSL::SSLSocket.new(sock)
@@ -23,8 +26,8 @@ Net::SMTP.class_eval do
       @socket.read_timeout = 60 #@read_timeout
       do_helo(helodomain)
     end
-
-    authenticate user, secret, authtype if user
+ 
+    authenticate user, secret, authtype
     @started = true
   ensure
     unless @started
@@ -33,7 +36,13 @@ Net::SMTP.class_eval do
       @socket = nil
     end
   end
-
+ 
+  def authenticate(user, secret, authtype = DEFAULT_AUTH_TYPE)
+    check_auth_method authtype
+    check_auth_args user, secret
+    send auth_method(authtype), user, secret
+  end
+ 
   def do_helo(helodomain)
     begin
       if @esmtp
@@ -50,12 +59,31 @@ Net::SMTP.class_eval do
       raise
     end
   end
-
+ 
+  def check_auth_args( user, secret)
+    unless user
+      raise ArgumentError, 'SMTP-AUTH requested but missing user name'
+    end
+    unless secret
+      raise ArgumentError, 'SMTP-AUTH requested buy missing secrete phrase'
+    end
+  end
+   
+  def check_auth_method(type)
+      unless respond_to?(auth_method(type), true)
+      raise ArgumentError, "wrong authentication type #{type}"
+    end
+  end
+ 
+  def auth_method(type)
+    "auth_#{type.to_s.downcase}".intern
+  end
+ 
   def starttls
     getok('STARTTLS') rescue return false
     return true
   end
-
+ 
   def quit
     begin
       getok('QUIT')
